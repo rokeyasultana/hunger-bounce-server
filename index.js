4ef39a11fb52b810482639c80ab7ecf86785bf21
jakiasultana2022 <rokeyasbd@gmail.com>
2023-02-21 17:32:15 +0600

    db connected

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require('express')
-const app = express()
-const port = 5000
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, CURSOR_FLAGS } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//mongodb
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.niidmnq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri);
+
+async function run (){
+try{
+   
+}
+
+finally{
+
+}
+
+
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello From Hunger Bounce!')
